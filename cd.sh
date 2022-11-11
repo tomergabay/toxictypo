484503067751358e68e3c8847f7fac358ca4eb81
tomer gaby <tomeriko01012001@gmail.com>
2022-11-11 11:05:58 +0000

    Update cd.sh

--- cd.sh
+++ cd.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-echo "deplys"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~deplying~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 #connect to the instance, cleanup the machine, extract the files from the .tar and execute the app
-# ssh -i "~/lavagna-gab.pem" ubuntu@$2 << EOF
-# docker rm -f /application
-# aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/c7o8u9c1
-# docker pull c7o8u9c1.dkr.ecr.us-east-1.amazonaws.com/toxicapp:${env.BUILD_NUMBER}
-# docker run --name application -p 8080:8080 toxicapp:${env.BUILD_NUMBER}
-# EOF
-
+ssh -i "~/lavagna-gab.pem" ubuntu@$1 << EOF
+docker rm -f /application
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/c7o8u9c1
+docker pull c7o8u9c1.dkr.ecr.us-east-1.amazonaws.com/toxicapp:${env.BUILD_NUMBER}
+docker run --name application -p 8080:8080 toxicapp:${env.BUILD_NUMBER}
+EOF
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~deplyed~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
