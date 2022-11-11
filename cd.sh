41bda146609b5cbbdae27d6641fc826dca77169e
tomer gaby <tomeriko01012001@gmail.com>
2022-11-11 12:04:21 +0000

    Update cd.sh

--- cd.sh
+++ cd.sh
@@ -7,7 +7,7 @@ ssh ubuntu@$1 << EOF
 docker rm -f /application
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/c7o8u9c1
 docker pull public.ecr.aws/c7o8u9c1/toxicapp:$2
-docker run --name application -p 8080:8080 public.ecr.aws/c7o8u9c1/toxicapp:$2
+docker run -d --name application -p 8080:8080 public.ecr.aws/c7o8u9c1/toxicapp:$2
 EOF
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~deplyed~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
